4eb8b13d7a42ca66062df7ed9a8d6e4bc21d9d22
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-12-07 10:24:42 -0200

    ➕ Add OneSignal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+	componentDidMount() {
+		const script = document.createElement("script");
+
+		script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
+		script.async = true;
+		script.onload = () => {
+			var OneSignal = window.OneSignal || [];
+			OneSignal.push(function() {
+				OneSignal.init({
+					appId: "8a7131ac-893b-4d80-b17d-74ee13cbe031",
+					allowLocalhostAsSecureOrigin: true
+				});
+			});
+		}
+
+		document.body.appendChild(script);
+	}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+				<div className='onesignal-customlink-container'></div>
       </div>
     );
   }
